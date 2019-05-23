80c54154d7846b374a27a8ed374cc44776dcd757
Brandon Baker <assgnomergb@gmail.com>
2019-05-22 23:30:37 -0700

    camera fix

--- Assets/cameracontrol.cs
+++ Assets/cameracontrol.cs
@@ -6,22 +6,23 @@ using UnityEngine.SceneManagement;
 public class cameracontrol : MonoBehaviour
 {
     public GameObject Player;
-    public Animator CamMovement;
+    public GameObject Cam;
 
     private void Update()
     {
         Vector2 Play = Player.transform.position;
+        Vector2 cam = Cam.transform.position;
         if (Player.transform.position.y >= -5f && Player.transform.position.y <= 5f && Player.transform.position.x > -8.95f && Player.transform.position.x < 8.95f)
         {
-            CamMovement.SetInteger("Screen", 1);
+            Cam.transform.position = new Vector3(0f, 0, -10);
         }
         if (Player.transform.position.y >= -5f && Player.transform.position.y <= 5f && Player.transform.position.x > 8.95f && Player.transform.position.x < 26.717f)
         {
-            CamMovement.SetInteger("Screen", 2);
+            Cam.transform.position = new Vector3(17.8082f, 0, -10);
         }
         if (Player.transform.position.y >= -5f && Player.transform.position.y <= 5f && Player.transform.position.x > 26.717f && Player.transform.position.x < 35.618f)
         {
-            CamMovement.SetInteger("Screen", 3);
+            Cam.transform.position = new Vector3(35.6164f, 0, -10);
         }
     }
 }
